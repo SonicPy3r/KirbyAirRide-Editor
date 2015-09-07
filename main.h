b5b6b82fbe422f67ebd92a75e0afa6a7d440b53f
TheInferno <smirkogm@gmail.com>
2015-09-07 16:44:46 +0200

    Create main.h

---
+++ main.h
@@ -0,0 +1,15 @@
+#ifndef _main_h_
+#define _main_h_
+
+    int image(int img);
+
+    int unsigned    header1 = 0x00;
+    int             header2 = 0x04;
+    int unsigned    header3 = 0x08;
+    int unsigned    header4 = 0x0C;
+    int unsigned    header5 = 0x10;
+    int             header6 = 0x14;
+    int unsigned    header7 = 0x18;
+    int unsigned    header8 = 0x1C;
+
+#endif
